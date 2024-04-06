75bc04385290b99ca13191aeeffee6b8e4cc2be6
catatsuy <catatsuy@catatsuy.org>
2024-04-06 15:26:59 +0900

    Update file path variable to hold multiple file paths

--- internal/cli/export_test.go
+++ internal/cli/export_test.go
@@ -1,13 +1,16 @@
 package cli
 
-import "regexp"
+import (
+	"io"
+	"regexp"
+)
 
-func (c *CLI) ReplaceProcess(searchPattern string, replacement string) error {
-	return c.replaceProcess(searchPattern, replacement)
+func (c *CLI) ReplaceProcess(searchPattern string, replacement string, inputStream io.Reader) error {
+	return c.replaceProcess(searchPattern, replacement, inputStream)
 }
 
-func (c *CLI) FilterProcess(filters []*regexp.Regexp, notFilters []*regexp.Regexp) error {
-	return c.filterProcess(filters, notFilters)
+func (c *CLI) FilterProcess(filters []*regexp.Regexp, notFilters []*regexp.Regexp, inputStream io.Reader) error {
+	return c.filterProcess(filters, notFilters, inputStream)
 }
 
 func CompileRegexps(rawPatterns []string) ([]*regexp.Regexp, error) {
